107c3fe642e0240843089827f2613b74bcb9e8be
Colin Mullikin <colin.mullikin@gmail.com>
2017-03-28 21:26:10 -0400

    Added moving monsters

--- FirstGame/GameScene.swift
+++ FirstGame/GameScene.swift
@@ -17,10 +17,41 @@ class GameScene: SKScene {
         backgroundColor = SKColor.white
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         addChild(player)
+        
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(addMonster),
+                SKAction.wait(forDuration: 1.0)
+            ])
+        ))
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func random() -> CGFloat{
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster(){
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        addChild(monster)
+        
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2,y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
 }
